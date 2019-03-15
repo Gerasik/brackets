3dd5de0c8d114a7252dbf1bcaf0ea61bb7907fd3
Gerasik <gerasik1992@gmail.com>
2019-03-15 17:04:07 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
+// module.exports = 
+function check(str, bracketsConfig) {
   // your solution
+  var result;
+  str = str.split('');
+  for (var j = 0; j<bracketsConfig.length ; j++)
+    for (var i = 0; i<str.length; i++){
+        if (str[i] == bracketsConfig[j][1] && str[i-1] == bracketsConfig[j][0]){
+        str.splice(i-1,2);
+      }
+  }
+  result=str;
+  
+  
+  return result;
 }
+
+console.log(check('((()))()',[['(', ')']]));//true
+console.log(check('())(',[['(', ')']]));//false 
